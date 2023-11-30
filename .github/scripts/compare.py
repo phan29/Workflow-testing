b47429039095d53fbb8bc3a05710a2292858aeb9
phan29 <151904766+phan29@users.noreply.github.com>
2023-11-30 14:50:52 +0530

    Update compare.py

--- .github/scripts/compare.py
+++ .github/scripts/compare.py
@@ -129,7 +129,7 @@ release_head_apk_name = sys.argv[2]
 debug_base_apk_name = sys.argv[3]
 debug_head_apk_name = sys.argv[4]
 
-apk_analyzer_path location will change based on the GHA runner that you're using i.e. mac/windows/ubuntu etc
+# apk_analyzer_path location will change based on the GHA runner that you're using i.e. mac/windows/ubuntu etc
 apk_analyzer_path = "/usr/local/lib/android/sdk/cmdline-tools/latest/bin/apkanalyzer"
 
 kb_in_bytes = 1024
