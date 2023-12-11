c3b54f25f4901f5d4cc8fe9358d36e383a7a9d65
phan29 <151904766+phan29@users.noreply.github.com>
2023-12-11 21:01:45 +0530

    Update temp.py

--- .github/scripts/temp.py
+++ .github/scripts/temp.py
@@ -3,5 +3,5 @@ import os
 env_file = os.getenv('GITHUB_ENV')
 
 with open(env_file, "a") as myfile:
-  myfile.write("old_apk_Size=3")
-  myfile.write("new_apk_Size=4")
+  myfile.write("old_apk_size=3")
+  myfile.write("new_apk_size=4")
