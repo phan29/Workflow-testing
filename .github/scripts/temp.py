9247838442211ecc6d57e06fbc186a48dd17e92b
phan29 <151904766+phan29@users.noreply.github.com>
2023-12-13 11:30:20 +0530

    Update temp.py

--- .github/scripts/temp.py
+++ .github/scripts/temp.py
@@ -6,6 +6,9 @@ with open(env_file, "a") as myfile:
   myfile.write("old_apk_size=3\n")
   myfile.write("new_apk_size=4")
 
-with open("apk_size_report.html", "w") as file:
+with open("apk_size_analyis_report.html", "w") as file:
   file.write("<html><body><h1>APK report</h1><h3>Affected products</h3></body></html>")
+  
+with open("aar_size_analysis_report.html", "w") as file:
+  file.write("<html><body><h1>AAR report</h1><h3>Affected products</h3></body></html>")
   
