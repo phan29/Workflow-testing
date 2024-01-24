22cf4438b54d25263db4222a31fed67516345fd3
phan29 <151904766+phan29@users.noreply.github.com>
2024-01-24 11:05:16 +0530

    Update temp.py

--- .github/scripts/temp.py
+++ .github/scripts/temp.py
@@ -1,6 +1,10 @@
 def solve():
   content = "<h2>Help</h2><br />"
-  content += "Happy"
+  content += f"<b>Base <a href='https://github.com/phan29/Workflow-testing/commit/{base_commit_sha}'>{base_commit_sha}</a></b><br>"
+  content += f"<b>Head <a href='https://github.com/phan29/Workflow-testing/commit/{head_commit_sha}'>{head_commit_sha}</a></b>"
   print(content)
 
+base_commit_sha = sys.argv[1]
+head_commit_sha = sys.argv[2]
+pr_number = sys.argv[3]
 solve()
